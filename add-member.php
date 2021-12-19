795489c67899ab277b22e73cda0d21a631b83aba
Aakash <aakashspardeshi02@gmail.com>
2021-12-20 01:44:37 +0530

    Added Member, List Member

---
+++ add-member.php
@@ -0,0 +1,20 @@
+<?php 
+    include('config.php');
+
+    $query = $pdo->prepare("
+    Insert into member (m_name,dob,city,state,street_no,street_name,apt_no)
+     VALUES (:mname,:dob,:city,:state,:streetNo,:streetName,:aptNo,:pin)"
+    );
+
+    
+    $query->bindParam('mname', $_POST['mname']);
+    $query->bindParam('dob', $_POST['dob']);
+    $query->bindParam('city', $_POST['city']);
+    $query->bindParam('state', $_POST['state']);
+    $query->bindParam('streetNo', $_POST['streetNo']);
+    $query->bindParam('streetName', $_POST['streetName']);
+    $query->bindParam('aptNo', $_POST['aptNo']);
+    $query->bindParam('pin', $_POST['pin']);
+    $query->execute();
+    header("location:list-member.php");
+?> 
